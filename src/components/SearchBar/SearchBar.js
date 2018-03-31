afda1abdd4c00edded0e673f79cfa4a4739c575c
Haldun Anil <haldunanil@gmail.com>
2018-03-31 15:23:21 -0400

    Check to make sure that all states are truthy

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -49,7 +49,9 @@ class SearchBar extends React.Component {
 	}
 
 	handleSearch(event) {
-		this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+		if (this.state.term && this.state.location && this.state.sortBy) {
+			this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+		}
 		event.preventDefault();
 	}	
 
