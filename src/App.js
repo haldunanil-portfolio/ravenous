e73d7b30d34e4037106883da9b489ecabdc5b50c
Haldun Anil <haldunanil@gmail.com>
2018-03-31 15:20:52 -0400

    Delete to do for 'Enter'

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ class App extends Component {
 	- Make images clickable and have them open the business' website in a new tab
 	- Clicking on a different sorting option automatically requeries the Yelp API, rather than having to manually click "Let's Go" again
 	- Implement your own type of sort (for example, by entering a distance or radius from a central location)
-	- Allow you to search by pressing "Enter" (or "Return") on your keyboard, as opposed to manually clicking
 	- Add autocompletion of addresses to the "Location" input
 
 	*/
